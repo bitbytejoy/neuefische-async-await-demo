f66be9647d9c6e555238a7bf5eb57a7960840081
bitbytejoy <bitbytejoy@gmail.com>
2022-12-14 14:09:31 +0100

    init: BIG BANG

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+
+type Character = {
+  id: number;
+  name: string;
+  status: string;
+  image: string;
+}
+
+// setInterval(() => {
+//   console.log("A");
+// }, 1);
+
+async function getRickAndMortyCharacter (): Promise<Character[]> {
+  try {
+    const response = await axios.get(
+      "https://rickandmortyapi.com/api/character"
+    );
+    return response.data.results;
+  } catch (e) {
+    throw e;
+  }
+}
 
 function App() {
+  console.log("App");
+  const [name, setName] = useState<string>("");
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const characters = await getRickAndMortyCharacter();
+        setCharacters(characters);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +55,10 @@ function App() {
         >
           Learn React
         </a>
+
+        <div>
+          <input value={name} onChange={e => setName(e.currentTarget.value)}/>
+        </div>
       </header>
     </div>
   );
